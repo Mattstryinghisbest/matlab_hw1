65deddabf0d3789d834e016b202893fcd45e5056
Mattstryinghisbest <Squill94@rowan.edu>
2024-09-27 15:53:31 +0000

    finished task 1

---
+++ homework_1/task_1.m
@@ -0,0 +1,28 @@
+% Author Name: Matthew Squillante
+% Email: Squill94@rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Assignment 2
+
+%Celcius to Farenhight Coverter
+
+% promt user to imput temp in celcius 
+
+c = input('Enter the temperature in Celsius: ');
+
+% Convert c to f 
+
+F = ( c * 9/5) + 32;
+
+fprintf('The temperature in Fahrenheit is %.2f°F\n' , F);
+
+%check if temp in f is above 100°F and display message if F>100
+
+    disp(' it''s a hot day!');
+
+% git add homework_1/tast_1.m
+% git comit -m 'finish tast 1'   
+% git push origin main         
+
+
+
+
